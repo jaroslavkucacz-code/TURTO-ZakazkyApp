64f8bbacc1d188ff63272e710a8d79bcdcb9e218
jaroslavkucacz-code <jaroslav.ku.ca.cz@gmail.com>
2026-09-10 13:39:39 +0200

    Load TURTO CRM 8.0.3 runtime optimization last

--- ZakazkyApp_base_6.1/runtime_bootstrap.py
+++ ZakazkyApp_base_6.1/runtime_bootstrap.py
@@ -66,6 +66,7 @@ LATE_LAYERS = (
     "price_lists_domain.platform.startup_optimization",
     "price_lists_domain.platform.ui_polish_801",
     "price_lists_domain.platform.branding_polish_802",
+    "price_lists_domain.platform.runtime_optimization_803",
 )
 
 
