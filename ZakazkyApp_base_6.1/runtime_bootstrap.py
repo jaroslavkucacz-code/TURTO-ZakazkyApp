fb15a076b2b0b177a58b56ada903e53e720c1286
jaroslavkucacz-code <jaroslav.ku.ca.cz@gmail.com>
2026-09-12 22:29:11 +0200

    fix: install frozen offer engine loader

--- ZakazkyApp_base_6.1/runtime_bootstrap.py
+++ ZakazkyApp_base_6.1/runtime_bootstrap.py
@@ -70,6 +70,7 @@ LATE_LAYERS = (
     "price_lists_domain.platform.icon_assets_803",
     "price_lists_domain.platform.idle_cleanup_804",
     "price_lists_domain.platform.autocomplete_event_compat_803",
+    "price_lists_domain.platform.frozen_offer_engine_806",
     "price_lists_domain.platform.action_archive_cleanup_806",
     "price_lists_domain.platform.operational_refresh_804",
 )
